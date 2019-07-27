b7744719072377622b52aa84478c0a08ff60d2c6
Graham Fairweather <xotic750@gmail.com>
2019-07-28 00:19:46 +0200

    Refactoring

--- dist/string-ends-with-x.js
+++ dist/string-ends-with-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-27T15:29:37.615Z",
+  "date": "2019-07-27T22:19:40.597Z",
   "describe": "",
   "description": "Determines whether a string ends with the characters of a specified string.",
   "file": "string-ends-with-x.js",
