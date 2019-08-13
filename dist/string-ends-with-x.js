099c67910933ed732a9d69d0467e63da9974bc74
Graham Fairweather <xotic750@gmail.com>
2019-08-13 17:55:40 +0200

    Auto run

--- dist/string-ends-with-x.js
+++ dist/string-ends-with-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-13T12:15:27.169Z",
+  "date": "2019-08-13T15:55:33.552Z",
   "describe": "",
   "description": "Determines whether a string ends with the characters of a specified string.",
   "file": "string-ends-with-x.js",
